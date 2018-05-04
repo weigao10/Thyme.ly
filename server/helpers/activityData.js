1e827248a95f9d7c5df9a753109321d2c78c58cc
brianlee <blees.email@gmail.com>
2018-05-04 14:15:21 -0400

    Created activity monitor

---
+++ server/helpers/activityData.js
@@ -0,0 +1,48 @@
+/*Leave this file running in the backgroud in Sublime text to get example data
+printed in your console
+*/
+const activeWin = require('active-win');
+const moment = require('moment');
+
+let activities = [];
+let errors = []; //NOT SURE IF ERROR IS DETECTED WHEN SCREEN JUST GOES BLANK
+
+const timestamp = () => { //can change moment format for ease of manipulation
+  return moment().format('MMMM Do YYYY, h:mm:ss a');
+};
+
+let counter = 0;
+const monitor = async () => {
+  try {
+    let activity = await activeWin();
+    activity.time = timestamp();
+    activities.push(activity);
+    counter++;
+    if (counter % 1000 === 0) { //control how often this prints to your console
+      console.log('just assuring you that monitor is still running', activities);
+    }
+  } catch(e) {
+    counter++;
+    e.time = timestamp();
+    errors.push(JSON.stringify(e)); //TODO: this loses some info but full error objects apparently can't be stored in an array
+    if (counter % 1000 === 0) {
+      console.log('just assuring you that monitor is still running', errors);
+    }
+  }
+};
+
+let workerId;
+const getTestData = (interval) => {
+  workerId = setInterval(monitor, interval);
+};
+const stopMonitor = () => {
+  //print activities and errors
+  console.log('activities for this session are', JSON.stringify(activities));
+  console.log('errors for this session are', JSON.stringify(errors));
+  clearInterval(workerId);
+}
+
+// getTestData(10000);
+
+exports.getTestData = getTestData;
+exports.stopMonitor = stopMonitor;
