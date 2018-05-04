f130b8097dcb2b6e4275420678dd6bd633616b8a
brianlee <blees.email@gmail.com>
2018-05-03 22:32:41 -0400

    Configured webpack

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import DashboardView from './components/DashboardView.jsx'
+import React from 'react';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,15 +12,15 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <Router>
-          <div>
-            <Route exact={true} path='/' component={(props) => <DashboardView /> }/>
-            {/* <Route path='/registration' component={(props) => <RegistrationPage /> }/> */}
-          </div>
-        </Router>
-      </div>
+    return ( <div>we haz webpack</div>
+      // <div>
+      //   <Router>
+      //     <div>
+      //       <Route exact={true} path='/' component={(props) => <DashboardView /> }/>
+      //       {/* <Route path='/registration' component={(props) => <RegistrationPage /> }/> */}
+      //     </div>
+      //   </Router>
+      // </div>
     )
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
+const port = process.env.PORT || 3000;
 
-let server = app.listen(3000, () => {
-  console.log('listening on port 3000!');
+app.use(express.static(path.join(__dirname, '/../react-client/dist')));
+app.use(bodyParser.json());
+
+app.get('/test', (req, res) => {
+  console.log('just seeing how webpack-dev-server works');
+  res.send('test');
+})
+
+let server = app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
