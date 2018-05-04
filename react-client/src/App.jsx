4a8ba0d5e4b8b5113d6330845900b9865b55794d
Wei Gao <wei.gao42@gmail.com>
2018-05-04 11:42:39 -0400

    separates stateful and stateless components

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import DashboardView from './components/DashboardView.jsx'
+import DashboardView from './containers/DashboardView.jsx'
 import React from 'react';
 
 class App extends React.Component {
@@ -12,15 +12,15 @@ class App extends React.Component {
   }
 
   render() {
-    return ( <div>we haz webpack</div>
-      // <div>
-      //   <Router>
-      //     <div>
-      //       <Route exact={true} path='/' component={(props) => <DashboardView /> }/>
-      //       {/* <Route path='/registration' component={(props) => <RegistrationPage /> }/> */}
-      //     </div>
-      //   </Router>
-      // </div>
+    // return ( <div>we haz webpack</div>
+    return(  <div>
+        <Router>
+          <div>
+            <Route exact={true} path='/' component={(props) => <DashboardView /> }/>
+            {/* <Route path='/registration' component={(props) => <RegistrationPage /> }/> */}
+          </div>
+        </Router>
+      </div>
     )
   }
 }
---
+++ react-client/src/components/DashboardView/Activity.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Activity = (props) => (<div>
+  Activity
+</div>)
+
+export default Activity;
