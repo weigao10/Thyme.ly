36ff248d429063e7aea1b6153ed9877442e7d2a1
brianlee <blees.email@gmail.com>
2018-05-04 14:15:21 -0400

    Created activity monitor

--- server/index.js
+++ server/index.js
@@ -3,9 +3,23 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+const { getTestData, stopMonitor } = require('./helpers/activityData.js');
 
 app.use(express.static(path.join(__dirname, '/../react-client/dist')));
 app.use(bodyParser.json());
+
+app.get('/starttest', (req, res) => {
+  getTestData(1000);
+  console.log('started up activity monitor!');
+  res.send('started up activity monitor!');
+});
+
+app.get('/stoptest', (req, res) => {
+  stopMonitor();
+  console.log('stopped activity monitor!');
+  res.send('stopped activity monitor!');
+})
+
 
 let server = app.listen(port, () => {
   console.log(`listening on port ${port}`);
