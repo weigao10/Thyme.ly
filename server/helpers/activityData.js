d6e25bc72b41280e4d9ba42b43422c109cd4ed0b
brianlee <blees.email@gmail.com>
2018-05-05 12:25:19 -0400

    Created routes and functions to get activity data

--- server/helpers/activityData.js
+++ server/helpers/activityData.js
@@ -1,50 +1,57 @@
-/*Leave this file running in the backgroud in Sublime text to get example data
-printed in your console
-*/
+//file to get sample data chunks
 const activeWin = require('active-win');
 const moment = require('moment');
-
+//closure variables
 let activities = [];
-let errors = []; //NOT SURE IF ERROR IS DETECTED WHEN SCREEN JUST GOES BLANK
+let errors = [];
 
 const timestamp = () => { //can change moment format for ease of manipulation
   return moment().format('MMMM Do YYYY, h:mm:ss a');
 };
 
-let counter = 0; //closure variable for monitor
+const assembleActivity = (activeWinObj) => {
+  return {
+    id: activeWinObj.id,
+    app: activeWinObj.owner.name,
+    title: activeWinObj.title
+  };
+};
+
 const monitor = async () => {
   try {
-    let activity = await activeWin();
-    activity.time = timestamp();
-    activities.push(activity);
-    counter++;
-    if (counter % 1000 === 0) { //control how often this prints to your console
-      console.log('just assuring you that monitor is still running', activities);
+    let newActivity = assembleActivity(await activeWin());
+    let lastActivity = activities[activities.length - 1];
+    //check to see if a new activity chunk is needed or if the most recent chunk needs to be updated
+    if (!lastActivity || (newActivity.app !== lastActivity.app
+      || newActivity.title !== lastActivity.title)) {
+      const time = timestamp();
+      if (lastActivity) {
+        lastActivity.endTime = time;
+      }      
+      newActivity.startTime = time;
+      activities.push(newActivity);
     }
   } catch(e) {
-    counter++;
     e.time = timestamp();
-    console.log('error is', e);
-    console.log('error message is', e.message);
-    errors.push(JSON.stringify(e)); //TODO: this loses some info but full error objects apparently can't be stored in an array
-    if (counter % 1000 === 0) {
-      console.log('just assuring you that monitor is still running', errors);
-    }
+    e.description = e.message; //not sure why I need to do this
+    errors.push(e); //TODO: this loses some info but full error objects apparently can't be stored in an array
   }
 };
 
-let workerId; //closure variable for below functions
-const getTestData = (interval) => {
-  workerId = setInterval(monitor, interval);
+let intervalId; //closure variable for below functions
+const startMonitor = (interval) => {
+  activities = [];
+  errors = [];
+  intervalId = setInterval(monitor, interval);
 };
 
 const stopMonitor = () => {
-  //assemble data ()
-  //print activities and errors
+  activities[activities.length - 1].endTime = timestamp();
   console.log('activities for this session are', JSON.stringify(activities));
   console.log('errors for this session are', JSON.stringify(errors));
-  clearInterval(workerId);
+  clearInterval(intervalId);
+  return JSON.stringify(activities);
 };
 
-exports.getTestData = getTestData;
+exports.startMonitor = startMonitor;
 exports.stopMonitor = stopMonitor;
