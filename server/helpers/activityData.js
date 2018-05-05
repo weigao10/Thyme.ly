0ef9474111a3e6efc468aff7e4d2e37e9c05230c
brianlee <blees.email@gmail.com>
2018-05-05 16:17:47 -0400

    Able to send activities via socket

--- server/helpers/activityData.js
+++ server/helpers/activityData.js
@@ -1,6 +1,7 @@
 //file to get sample data chunks
 const activeWin = require('active-win');
 const moment = require('moment');
+
 //closure variables
 let activities = [];
 let errors = [];
@@ -38,12 +39,38 @@ const monitor = async () => {
   }
 };
 
+const monitorSocket = async () => {
+  try {
+    let newActivity = assembleActivity(await activeWin());
+    let lastActivity = activities[activities.length - 1];
+    //check to see if a new activity chunk is needed or if the most recent chunk needs to be updated
+    if (!lastActivity || (newActivity.app !== lastActivity.app
+      || newActivity.title !== lastActivity.title)) {
+      const time = timestamp();
+      if (lastActivity) {
+        lastActivity.endTime = time;
+      }      
+      newActivity.startTime = time;
+      console.log('about to emit');
+      io.sockets.emit('new activity', {activity: 'HI'})
+      activities.push(newActivity);
+    }
+  } catch(e) {
+    e.time = timestamp();
+    e.description = e.message; //not sure why I need to do this
+    errors.push(e); //TODO: this loses some info but full error objects apparently can't be stored in an array
+  }
+};
+
 let intervalId; //closure variable for below functions
 const startMonitor = (interval) => {
   activities = [];
   errors = [];
   intervalId = setInterval(monitor, interval);
 };
+
+
+
 
 const stopMonitor = () => {
   activities[activities.length - 1].endTime = timestamp();
@@ -53,5 +80,47 @@ const stopMonitor = () => {
   return JSON.stringify(activities);
 };
 
-exports.startMonitor = startMonitor;
-exports.stopMonitor = stopMonitor;
+const connectToSocket = () => {
+  const {io} = require('../index.js');
+  io.on('connection', (socket) => {
+    const startSocketMonitor = (interval) => {
+
+      const monitorSocket = async () => {
+        try {
+          let newActivity = assembleActivity(await activeWin());
+          let lastActivity = activities[activities.length - 1];
+          //check to see if a new activity chunk is needed or if the most recent chunk needs to be updated
+          if (!lastActivity || (newActivity.app !== lastActivity.app
+            || newActivity.title !== lastActivity.title)) {
+            const time = timestamp();
+            if (lastActivity) {
+              lastActivity.endTime = time;
+            }      
+            newActivity.startTime = time;
+            console.log('about to emit');
+            io.sockets.emit('new activity', {activity: newActivity})
+            activities.push(newActivity);
+          }
+        } catch(e) {
+          e.time = timestamp();
+          e.description = e.message; //not sure why I need to do this
+          errors.push(e); //TODO: this loses some info but full error objects apparently can't be stored in an array
+        }
+      };
+      
+      activities = [];
+      errors = [];
+      intervalId = setInterval(monitorSocket, interval);
+    };
+
+    console.log('connected to socket inside helper!');
+    io.sockets.emit('new activity', {activity: 'HI'})
+    startSocketMonitor();
+  });
+
+
+};
+
+// exports.startMonitor = startMonitor;
+// exports.stopMonitor = stopMonitor;
+exports.connectToSocket = connectToSocket;
