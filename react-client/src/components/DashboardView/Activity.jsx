7602e9ac4d44c53f3a2705816707fdf5aa471c75
brianlee <blees.email@gmail.com>
2018-05-07 17:27:55 -0400

    Reorganized state into productive, neutral, and distracting activity arrays

--- react-client/src/components/DashboardView/Activity.jsx
+++ react-client/src/components/DashboardView/Activity.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
+const renderActivities = (category, activities) => {
+  return (
+    <div>
+      <h4>{category}</h4>
+      {activities[category].map((activity) => {
+        return <div>
+          app: {activity.app} <br/>
+          window_title: {activity.title} &nbsp;
+        </div>
+      })}
+    </div>
+  )
+}
+
 const Activity = ({activities}) => {
   console.log('activities in activity.jsx', activities)
-return (<div>
-  {activities.activities.map((activity) => {
-    return <div>app: {activity.app} <br/>
-                window_title: {activity.title} &nbsp;
-                </div>
-  })}
-</div>)}
+  return (
+    <div>
+      {renderActivities('neutral', activities)}
+      {renderActivities('productive', activities)}
+      {renderActivities('distracting', activities)}
+    </div>
+  )
+}
 
 export default Activity;
