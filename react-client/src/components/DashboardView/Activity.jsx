5c291163f2ebca5103d0c1d45b6863feec6ca407
Wei Gao <wei.gao42@gmail.com>
2018-05-07 17:31:25 -0400

    refactors code to use redux

--- react-client/src/components/DashboardView/Activity.jsx
+++ react-client/src/components/DashboardView/Activity.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Activity = ({activities}) => {
-  console.log('activities in activity.jsx', activities)
-return (<div>
+const Activity = ({activities}) => (<div>
   {activities.activities.map((activity) => {
     return <div>app: {activity.app} <br/>
                 window_title: {activity.title} &nbsp;
                 </div>
   })}
-</div>)}
+</div>)
 
-export default Activity;
+
+const mapStateToProps = state => ({
+  activities: state.activities
+})
+
+export default connect(mapStateToProps)(Activity) 
