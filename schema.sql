9da21beed82cd82a998a2f46ef78f3d2b2382f97
Andrew Lee <yenju.andrew.lee@gmail.com>
2018-05-07 15:29:09 -0400

    merge

--- schema.sql
+++ schema.sql
@@ -5,26 +5,26 @@ DROP TABLE pomodoro_pref;
 DROP TABLE errors;
 
 CREATE TABLE users(
-  user_id serial PRIMARY KEY,
-  username VARCHAR (20),
-  hash VARCHAR (20),
-  pom_id VARCHAR (20),
+  user_id serial NOT NULL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  hash VARCHAR(20),
+  pomodoro_id VARCHAR(20),
 );
 
 CREATE TABLE activities(
-  activity_id serial PRIMARY KEY,
+  activity_id serial NOT NULL PRIMARY KEY,
   start_time TIMESTAMP NOT NULL,
   end_time TIMESTAMP NOT NULL,
-  app_name VARCHAR(25),
-  window_title VARCHAR(50),
-  category VARCHAR(25),
-  prod_class VARCHAR(25),
-  url VARCHAR (100),
+  app_name VARCHAR NOT NULL,
+  window_title VARCHAR,
+  category VARCHAR,
+  prod_class VARCHAR,
+  url VARCHAR,
 );
 
 CREATE TABLE user_metrics(
   date TIMESTAMP not null,
-  prod_number integer (3),
+  prod_number integer,
 );
 
 CREATE TABLE pomodoro_pref(
@@ -36,7 +36,7 @@ CREATE TABLE pomodoro_pref(
 );
 
 CREATE TABLE errors(
-  error_id serial,
-  error_message VARCHAR(255)
+  error_id serial NOT NULL PRIMARY KEY,
+  error_message VARCHAR NOT NULL
 );
 
