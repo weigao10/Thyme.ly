f387ea994f036f82ea8d1d66ad8b9a53a4fd03c5
brianlee <blees.email@gmail.com>
2018-05-08 15:59:15 -0400

    Attempting a recategorize fn1

--- react-client/src/actions/activityActions.js
+++ react-client/src/actions/activityActions.js
@@ -1,5 +1,6 @@
 import { GET_ACTIVITIES, ADD_ACTIVITY, PATCH_ACTIVITY, DELETE_ACTIVITY } from './types'; 
 import thunk from 'redux-thunk';
+import store from '../store';
 
 export const getActivities = () => (dispatch) => {
   //axios request to db
@@ -9,12 +10,53 @@ export const getActivities = () => (dispatch) => {
   // })
 }
 
-export const addActivity = (data) => (dispatch) => {
-  // console.log('data in post', data.activity);
-  dispatch({
-    type: ADD_ACTIVITY,
-    payload: data.activity
-  })
+
+export const addActivity = (data) => (dispatch, getState) => {
+  const {activities} = getState()
+  let activityInState = checkState(data, activities)
+  if (activityInState){
+    console.log('already in state. update duration!')
+  } else {
+    console.log('not in state. update state!')
+    dispatch({
+      type: ADD_ACTIVITY,
+      payload: data.activity
+    })
+  }
+}
+
+const changeCategory = (activity, oldCat, newCat) => (dispatch, getState) => {
+  const {activities} = getState();
+  const {[oldCat]: oldCat, [newCat]: newCat} = activities; //destructuring using variable keys
+  //dispatch updated oldCat and newCat pieces of the activities obj?
+  /*
+  payload : {
+    [oldCat]: updatedOld
+    [newCat]: updatedNew
+  }
+  */
+}
+
+const getActivity = (app, title) => {
+  for (let category in activities) {
+    for (let activity of activities[category]) {
+      if (activity.app === app && activity.title === title) {
+        return activity;
+      }
+    }
+  }
+  return false;
+}
+
+const checkState = (data, activities) => {
+  for (let category in activities) {
+    for (let activity of activities[category]) {
+      if (activity.title === data.activity.title && activity.app === data.activity.app) {
+        return true
+      }
+    }
+  }
+  return false;
 }
 
 
