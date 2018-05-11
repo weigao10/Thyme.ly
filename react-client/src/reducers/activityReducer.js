25df47ab8477e4e62afa94453006ac02433e6ee2
brianlee <blees.email@gmail.com>
2018-05-11 18:53:09 -0400

    Cleanup, deleted dummy initial data

--- react-client/src/reducers/activityReducer.js
+++ react-client/src/reducers/activityReducer.js
@@ -4,23 +4,11 @@ import { GET_ACTIVITIES, ADD_ACTIVITY, PATCH_ACTIVITY, CATEGORIZE_ACTIVITY } fro
 import moment from 'moment';
 
 const initialState = {
-  neutral: [
-      {'id': 0,
-      'app': 'System Preferences',
-      'title': 'Control Panel',
-      'spurts': [{'startTime': "May 7th 2018, 5:51:26 pm", 'endTime': "May 7th 2018, 5:51:46 pm"}],
-      'duration': 15},
-  ],
-  productive: [
-    {'id': 1, 'app': 'Google Chrome', 'title': 'Productivity Manager', 'spurts': [{'startTime': "May 7th 2018, 5:51:26 pm", 'endTime': "May 7th 2018, 5:51:46 pm"}], 'duration': 20},
-    {'id': 2,'app': 'Chrome', 'title': 'Youtube - WTF IS REDUX', 'spurts': [{'startTime': "May 7th 2018, 5:51:26 pm", 'endTime': "May 7th 2018, 5:51:46 pm"}], 'duration': 76},
-  ],
-  distracting: [
-    {'id': 3,'app': 'Google Chrome', 'title': 'Reddit', 'spurts': [{'startTime': "May 7th 2018, 5:51:26 pm", 'endTime': "May 7th 2018, 5:51:46 pm"}], 'duration': 45},
-    {'id': 4,'app': 'Google Chrome', 'title': 'Store - Redux', 'spurts': [{'startTime': "May 7th 2018, 5:51:26 pm", 'endTime': "May 7th 2018, 5:51:46 pm"}], 'duration': 108},
-  ],
-  nextId: 5
-}
+  neutral: [],
+  productive: [],
+  distracting: [],
+  nextId: 1
+};
 
 const activities = (state = initialState, action) => {
   // console.log('reached reducer');
@@ -28,11 +16,8 @@ const activities = (state = initialState, action) => {
     case GET_ACTIVITIES: 
       return {
         ...state, 
-        // activities: [... state.activities, action.payload]
       }
     case ADD_ACTIVITY:
-      // console.log('reducer add act', action.payload)
-      // console.log(state)
       let newData = {
         'id': state.nextId,
         'app': action.payload.app,
@@ -67,12 +52,10 @@ const activities = (state = initialState, action) => {
       }
       // return state
     case CATEGORIZE_ACTIVITY:
-      // console.log('got dispatch', action.payload)
       let {id, oldCatName, newCatName} = action.payload;
       const movingActivity = state[oldCatName].filter((el) => el.id === id)[0];
       const updatedOldCat = state[oldCatName].filter((el) => el.id !== id);
       const updatedNewCat = [...state[newCatName] , movingActivity];
-      // console.log('here is your new stuff in the reducer', movingActivity, updatedOldCat, updatedNewCat)
       return {
         ...state,
         [oldCatName]: updatedOldCat,
