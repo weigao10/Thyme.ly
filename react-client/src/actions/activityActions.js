1c4b2899a20b0ab8b4c0876f2259c537609bff49
brianlee <blees.email@gmail.com>
2018-05-11 14:13:47 -0400

    User categorization and recategorization of activities saving into DB

--- react-client/src/actions/activityActions.js
+++ react-client/src/actions/activityActions.js
@@ -2,6 +2,7 @@ import { GET_ACTIVITIES, ADD_ACTIVITY, PATCH_ACTIVITY, CATEGORIZE_ACTIVITY } fro
 import { createStore } from 'redux';
 import moment from 'moment';
 import axios from 'axios';
+const url = 'http://127.0.0.1:3000'
 
 export const getActivities = () => (dispatch) => {
   
@@ -22,13 +23,23 @@ export const patchActivity = ({ activity, category, index }, data) => {
   }
 }
 
-export const changeCategory = (id, oldCatName, newCatName) => {
-  console.log('activity action firing with', id, oldCatName, newCatName)
-  //get entire activity
+export const changeCategory = (activity, oldCatName, newCatName) => {
+  console.log('change cat action firing with', activity, oldCatName, newCatName)
+  //send off the username, apptitle, windowtitle to server
+  //return dat payload
+  const params = {
+    user_name: 'brian',
+    app_name: activity.app,
+    window_title: activity.title,
+    prod_class: newCatName
+  };
+  console.log('params are', params)
+  const request = axios.post(url + '/api/classifications', {params: params})
   return {
     type: CATEGORIZE_ACTIVITY,
     payload: {
-      id,
+      request,
+      id: activity.id,
       oldCatName,
       newCatName
     }
