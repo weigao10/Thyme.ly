99e8bbcce58b8739f4cd3a2049f6bc2fdaf25283
brianlee <blees.email@gmail.com>
2018-05-12 14:54:34 -0400

    Partial refactoring of activity components

--- react-client/src/containers/MonitorContainer.jsx
+++ react-client/src/containers/MonitorContainer.jsx
@@ -3,20 +3,30 @@ import React from 'react';
 import { ipcRenderer } from 'electron';
 import axios from 'axios';
 
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import Divider from 'material-ui/Divider';
+
 import { addActivity, patchActivity} from '../actions/activityActions.js';
 
 class MonitorContainer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showTimerButton: true
+    }
+
     this.connectMonitor = this.connectMonitor.bind(this);
     this.pauseMonitor = this.pauseMonitor.bind(this);
     this.checkState = this.checkState.bind(this);
+    this.toggleTimerButton = this.toggleTimerButton.bind(this);
   }
 
   componentDidMount() {
     this.connectMonitor();
     ipcRenderer.on('activity', (event, message) => {
-      console.log('getting a message at the monitor container!', message);
       const inState = this.checkState(message);
       this.props.activityHandler(message, inState);
     });
@@ -30,6 +40,18 @@ class MonitorContainer extends React.Component {
   pauseMonitor() {
     this.connected = false;
     ipcRenderer.send('monitor', 'pause');
+  }
+
+  toggleTimerButton() {
+    let toggle = !this.state.showTimerButton;
+    this.setState({
+      showTimerButton: toggle
+    });
+    if (!toggle) {
+      this.pauseMonitor();
+    } else {
+      this.connectMonitor();
+    }
   }
 
   checkState (data) {
@@ -50,10 +72,13 @@ class MonitorContainer extends React.Component {
 
   render() {
     return (
-      <div>HI I AM MonitorContainer
-      <pre>{JSON.stringify(
-        this.props.activities
-      )}</pre>
+      <div>
+        <AppBar 
+            title='Dashboard'
+            style={{background: '#2196F3', margin: '0px'}}
+            iconElementRight={this.state.showTimerButton ? <FlatButton label="Pause Tracker"/> : <FlatButton label="Restart Tracker" />}
+            onRightIconButtonClick={this.toggleTimerButton}
+          />
       </div>
     )
   }
@@ -63,7 +88,7 @@ const mapStateToProps = state => ({
   activities: state.activities
 });
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     activityHandler: (data, inState) => {
       if (inState) dispatch(patchActivity(inState, data))
