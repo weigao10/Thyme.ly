a92d72498a5f727eac251595e8108afe46f2e686
brianlee <blees.email@gmail.com>
2018-05-12 14:54:34 -0400

    Partial refactoring of activity components

--- react-client/src/components/DashboardView/ActivityGroup.jsx
+++ react-client/src/components/DashboardView/ActivityGroup.jsx
@@ -5,4 +5,23 @@ const ActivityGroup = () =>  (<div>
   <Activity/>
 </div>)
 
+const sortByDuration = (obj) => {
+  let newObj = clone(obj)
+  for(let key in newObj){
+    if(Array.isArray(newObj[key])) {
+      newObj[key].sort((a, b) => b.duration-a.duration)
+    }
+  }
+  return newObj
+}
+
+const getTotalDuration = (activities) => {
+  let duration = 0;
+  activities.forEach((activity) => {
+    duration += activity.duration;
+  })
+  let formatDuration = moment.duration(duration, "seconds").format("h[h], m[m] s[s]");
+  return formatDuration;
+}
+
 export default ActivityGroup;
