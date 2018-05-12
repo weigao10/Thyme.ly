bb16bc3139fb0ef1d6cfc840032396bd77e51c2d
Wei Gao <wei.gao42@gmail.com>
2018-05-12 17:01:59 -0400

    adds styling

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,26 +8,12 @@ $('.message a').click(function(){
   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 
-firebase.initializeApp(config);
-var provider = new firebase.auth.GoogleAuthProvider();
-
-var googleSignIn = () => {
-  firebase.auth().signInWithPopup(provider)
-  .then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-  })
-  .catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-  });
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
 }
+var provider = new firebase.auth.GoogleAuthProvider();
+var auth = firebase.auth();
+auth.signInWithPopup(provider); 
 
 var registerButton = document.getElementById('register')
 var loginButton = document.getElementById('login')
@@ -37,7 +23,7 @@ registerButton.addEventListener('click', () => {
   let email = document.getElementById('reg-email').value
   let password = document.getElementById('reg-password').value
 
-  firebase.auth().createUserWithEmailAndPassword(email, password)
+  auth.createUserWithEmailAndPassword(email, password)
   .then((data) => ReactDOM.render((<App />), document.getElementById('app')))
   .catch((err) => console.log('err in register: ', err))
 })
@@ -46,7 +32,7 @@ registerButton.addEventListener('click', () => {
 loginButton.addEventListener('click', () => {
   let email = document.getElementById('email').value
   let password = document.getElementById('password').value
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  auth.signInWithEmailAndPassword(email, password)
   .then((data) => ReactDOM.render((<App />), document.getElementById('app')))
   .catch((err) => alert('Username/password combination do not match.'))
 })
