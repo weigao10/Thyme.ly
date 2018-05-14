2920af3e9136020a129ba3afda6e37b4b90ed5dc
brianlee <blees.email@gmail.com>
2018-05-13 21:26:36 -0400

    Basic implementation of bayes classifier

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
-const db = require('../database/index.js');
+const db = require('./database/index.js');
+const naiveBayes = require('./learn/naiveBayes.js');
 
 // app.use(express.static(path.join(__dirname, '/../react-client/dist')));
 app.use(bodyParser.json());
@@ -13,13 +14,17 @@ app.get('/api/classifications', (req, res) => {
   return db.getProductivityClass(app_name, window_title)
     .then((prod_class) => res.send(prod_class))
     .catch((err) => res.send(err));
-})
+});
 
 app.post('/api/classifications', (req, res) => {
   return db.addOrChangeProductivity(req.body.params)
     .then(message => res.send(message))
     .catch(err => console.log(err))
-})
+});
+
+app.get('/learn', (req, res) => {
+  naiveBayes();
+});
 
 let server = app.listen(port, () => {
   console.log(`listening on port ${port}`);
