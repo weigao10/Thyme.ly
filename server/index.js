e76f61c95013794d8eb8e64950099d618caec5ae
brianlee <blees.email@gmail.com>
2018-05-16 18:10:40 -0400

    Commit before separating electron and server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,19 @@ const session = require('express-session');
 const partials = require('express-partials');
 const app = express();
 const port = process.env.PORT || 3000;
-const moment = require('moment')
+const moment = require('moment');
+const memwatch = require('memwatch-next');
+const chalk = require('chalk');
+
+memwatch.on('leak', function(info) {
+  console.log('LEAK INFO FROM MEMWATCH')
+  console.log(chalk.red.bgYellow(info))
+});
+
+memwatch.on('stats', function(stats) {
+  console.log('garbage collection info from memwatch')
+  console.log(stats)
+});
 
 const db = require('./database/index.js');
 // const naiveBayes = require('./learn/naiveBayes.js');
@@ -28,8 +40,9 @@ app.use(bodyParser.json());
 // };
 
 app.get('/api/classifications', (req, res) => {
+  const hd = new memwatch.HeapDiff();
   const {user_name, app_name, window_title} = req.query;
-  console.log('GET req is', req.query)
+  // console.log('GET req is', req)
   if (!user_name) {
     res.send('no user attached to this session')
   }
@@ -40,17 +53,26 @@ app.get('/api/classifications', (req, res) => {
       //   naiveBayes.predictProducitivityClass(window_title);
       // }
       res.send(prod_class);
+      const diff = hd.end();
+      console.log('MEMWATCH DIFF after get request')
+      console.log(diff)
     })
     .catch((err) => res.send(err));
 });
 
 app.post('/api/classifications', (req, res) => {
-  console.log('POST req is', req.body)
+  // console.log('POST req is', req)
+  const hd = new memwatch.HeapDiff();
   if (!req.body.params.user_name) {
     res.send('no user attached to this session')
   }
   return db.addOrChangeProductivity(req.body.params)
-    .then(message => res.send(message))
+    .then(message => {
+      res.send(message)
+      const diff = hd.end();
+      console.log('MEMWATCH DIFF after POST request')
+      console.log(diff)
+    })
     .catch(err => console.log(err))
 });
 
