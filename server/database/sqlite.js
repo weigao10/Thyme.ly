75e4e45b1ac085ab6d37c6ba9046628ba2737ef9
Wei Gao <wei.gao42@gmail.com>
2018-05-17 15:11:01 -0400

    saves spurts

--- server/database/sqlite.js
+++ server/database/sqlite.js
@@ -2,6 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const dbPath = path.resolve(__dirname, 'sqlite.db')
 const db = new sqlite3.Database(dbPath);
+const Promise = require('bluebird')
 
 
 //NEED TO ADD DATE COLUMN
@@ -15,41 +16,87 @@ const createTable = () => {
 
 //NEED TO ADD DATE COLUMN
 const insertActivities = ({id, productivity, app, title, duration}) => {
-  db.run(`INSERT INTO activities(id, productivity, app, title, duration) VALUES(?, ?, ?, ?, ?)`, 
-  [id, productivity, app, title, duration], 
-  (err) => {
-    if (err) return console.log('ERR IN SQLITE INSERT ACTIVITIES: ', err.message);
-    console.log(`A row has been inserted into activities`);
-  });
+  // db.run(`INSERT INTO activities(id, productivity, app, title, duration) VALUES(?, ?, ?, ?, ?)`, 
+  // [id, productivity, app, title, duration], 
+  // (err) => {
+  //   if (err) return console.log('ERR IN SQLITE INSERT ACTIVITIES: ', err.message);
+  //   console.log(`A row has been inserted into activities`);
+  // });
+  
+  let query = `INSERT INTO activities(id, productivity, app, title, duration) VALUES(?, ?, ?, ?, ?)`;
+  let params = [id, productivity, app, title, duration];
+  return new Promise ((resolve, reject) => {
+    db.run(query, params, (err, result) => {
+      if (err) reject(err)
+      else resolve(result)
+    })
+  })
 }
 
 const insertPreferences = (data, category) => {
-  db.run(`INSERT INTO preferences(data, category) VALUES(?, ?)`, 
-  [data, category], 
-  (err) => {
-    if (err) return console.log('ERR IN SQLITE INSERT PREFERENCES: ',err.message);
-    console.log(`A row has been inserted into preferences`);
-  });
+  // db.run(`INSERT INTO preferences(data, category) VALUES(?, ?)`, 
+  // [data, category], 
+  // (err) => {
+  //   if (err) return console.log('ERR IN SQLITE INSERT PREFERENCES: ',err.message);
+  //   console.log(`A row has been inserted into preferences`);
+  // });
+
+  let query = `INSERT INTO preferences(data, category) VALUES(?, ?)`;
+  let params = [data, category];
+  return new Promise ((resolve, reject) => {
+    db.run(query, params, (err, result) => {
+      if (err) reject(err)
+      else resolve(result)
+    })
+  })
 }
 
 //NEED TO ADD DATE
-const insertSpurts = ({id, productivity, app, title, startTime, endTime}) => {
-  db.run(`INSERT INTO spurts(id, productivity, app, title, startTime, endTime) VALUES(?, ?, ?, ?, ?, ?)`, 
-  [id, productivity, app, title, startTime, endTime], 
-  (err) => {
-    if (err) return console.log('ERR IN SQLITE INSERT SPURTS: ',err.message);
-    console.log(`A row has been inserted into spurts`);
-  });
+const insertSpurts = ({id, productivity, app, title}, {startTime, endTime}) => {
+  console.log('insert spurt');
+
+  // db.run(`INSERT INTO spurts(id, productivity, app, title, startTime, endTime) VALUES(?, ?, ?, ?, ?, ?)`, 
+  // [id, productivity, app, title, startTime, endTime], 
+  // (err) => {
+  //   if (err) return console.log('ERR IN SQLITE INSERT SPURTS: ',err.message);
+  //   console.log(`A row has been inserted into spurts`);
+  // });
+  let query = `INSERT INTO spurts(id, productivity, app, title, startTime, endTime) VALUES(?, ?, ?, ?, ?, ?)`;
+  let params = [id, productivity, app, title, startTime, endTime];
+  return new Promise ((resolve, reject) => {
+    db.run(query, params, (err, result) => {
+      if (err) reject(err)
+      else resolve(result)
+    })
+  })
 }
 
-const getActivities = (cb) => {
-    console.log("get activities");
-    let query = `SELECT id, productivity, app, title, duration FROM activities`;
-    let results;
-    db.all(query, [], (err, rows) => {
-      if (err) return console.log('ERR IN SQLITE GET ACTIVITIES: ',err.message);
-      cb(rows)
-    });
+const getActivities = () => {
+  console.log("get activities");
+  let query = `SELECT id, productivity, app, title, duration FROM activities`;
+
+  return new Promise ((resolve, reject) => {
+    db.all(query, [], (err, result) => {
+      if (err) reject(err)
+      else resolve(result)
+    })
+  })
+}
+
+const getSpurts = (cb) => {
+  console.log("get spurts");
+  let query = `SELECT id, productivity, app, title, startTime, endTime FROM spurts`;
+  // db.all(query, [], (err, rows) => {
+  //   if (err) return console.log('ERR IN SQLITE GET SPURTS: ',err.message);
+  //   cb(rows)
+  // });
+
+  return new Promise ((resolve, reject) => {
+    db.all(query, [], (err, result) => {
+      if (err) reject(err)
+      else resolve(result)
+    })
+  })
 }
 
 const clearDb = () => {
@@ -67,6 +114,8 @@ const closeDb = () => {
 exports.createTable = createTable;
 exports.insertActivities = insertActivities;
 exports.insertPreferences = insertPreferences;
+exports.insertSpurts = insertSpurts;
+exports.getSpurts = getSpurts;
 exports.getActivities = getActivities;
 exports.clearDb = clearDb;
 exports.closeDb = closeDb;
