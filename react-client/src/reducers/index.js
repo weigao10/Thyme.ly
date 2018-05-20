709cfdf920a71a4e8c1b2cbb363c98539e2be1eb
brianlee <blees.email@gmail.com>
2018-05-20 13:52:16 -0400

    Pomodoro incrementing logic implemented in Redux

--- react-client/src/reducers/index.js
+++ react-client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import activityReducer from './activityReducer.js';
 import preferencesReducer from './preferencesReducer.js';
 import userReducer from './userReducer.js';
+import pomodoroReducer from './pomodoroReducer.js';
 
 export default combineReducers({
   activities: activityReducer,
   preferences: preferencesReducer,
-  user: userReducer
+  user: userReducer,
+  pomodoro: pomodoroReducer
 })
