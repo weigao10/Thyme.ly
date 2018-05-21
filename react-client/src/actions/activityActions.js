2bd7ae1d43cd1649fe24dcaa16d39f93080052bd
Wei Gao <wei.gao42@gmail.com>
2018-05-21 10:31:08 -0400

    new changes

--- react-client/src/actions/activityActions.js
+++ react-client/src/actions/activityActions.js
@@ -1,4 +1,5 @@
-import { ADD_ACTIVITY, PATCH_ACTIVITY, CATEGORIZE_ACTIVITY, DELETE_ACTIVITY, SET_ALL_ACTIVITIES, AFFIRM_CATEGORIZATION } from './types'; 
+import { ADD_ACTIVITY, PATCH_ACTIVITY, CATEGORIZE_ACTIVITY, DELETE_ACTIVITY, SET_ALL_ACTIVITIES } from './types'; 
+// import { createStore } from 'redux';
 import moment from 'moment';
 import axios from 'axios';
 const url = 'http://127.0.0.1:3000'
@@ -11,7 +12,7 @@ export const addActivity = (data) => {
 }
 
 export const deleteActivity = (activity, category, isTracked, user) => {
-  // console.log('deleting this activity card isTracked is:', isTracked);
+  console.log('deleting this activity card isTracked is:', isTracked);
   let id = activity.id;
 
   const params = {
@@ -30,7 +31,6 @@ export const deleteActivity = (activity, category, isTracked, user) => {
 }
 
 export const patchActivity = ({ activity, category, index }, data) => {
-  //console.log('activity inside patchActivity is', activity)
   return {
     type: PATCH_ACTIVITY,
     payload: {activity, category, index, data}
@@ -38,7 +38,6 @@ export const patchActivity = ({ activity, category, index }, data) => {
 }
 
 export const changeCategory = (activity, oldCatName, newCatName, isTracked, user) => {
-  console.log('isTracked inside changeCat is', isTracked)
   const params = {
     user_name: user,
     app_name: activity.app,
@@ -57,25 +56,6 @@ export const changeCategory = (activity, oldCatName, newCatName, isTracked, user
       id: activity.id,
       oldCatName,
       newCatName
-    }
-  }
-}
-
-export const affirmCategorization = (activity, category, isTracked, user) => {
-  console.log('isTracked inside affirmCat is', isTracked)
-  const params = {
-    user_name: user,
-    app_name: activity.app,
-    window_title: activity.title,
-    prod_class: category,
-    isTracked: isTracked
-  };
-  const request = axios.post(url + '/api/classifications', {params: params});
-  return {
-    type: AFFIRM_CATEGORIZATION,
-    payload: {
-      request,
-      activity,
     }
   }
 }
