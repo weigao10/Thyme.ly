dced98b165bc4bef2b9a79ff9c95b53ed1259b83
Wei Gao <wei.gao42@gmail.com>
2018-05-22 11:15:08 -0400

    removes out

--- out/productivity-manager-darwin-x64/productivity-manager.app/Contents/Resources/app/react-client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import activityReducer from './activityReducer.js';
-import preferencesReducer from './preferencesReducer.js'
-
-export default combineReducers({
-  activities: activityReducer,
-  preferences: preferencesReducer
-})
