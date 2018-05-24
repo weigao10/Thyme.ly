a59519ec81ca3e9f1cfc7cc1e4a03a35d5883e7c
brianlee <blees.email@gmail.com>
2018-05-24 15:40:46 -0400

    Basic treechart

--- electron/react-client/src/components/ChartSpurts.jsx
+++ electron/react-client/src/components/ChartSpurts.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import { Treemap, Tooltip } from 'recharts';
 import moment from 'moment';
-
-const data = [
-  {name: '8am', uv: 280, pv: 450, amt: 360},
-];
-const data2 = [
-  {name: '9am', uv: 280, pv: 450, amt: 360},
-];
-const data3 = [
-  {name: '10am', uv: 280, pv: 450, amt: 360},
-];
-const data4 = [
-  {name: '11am', uv: 280, pv: 450, amt: 360},
-];
-const formatXAxis = function(tickItem) {
-  if (tickItem === 3600)
-    return ''
-
-  return ':' + tickItem / 60;
-}
 
 const timestampToUnix = (timeStamp) => {
   return moment(timeStamp, 'MMMM Do YYYY, h:mm:ss a').valueOf();
+};
+
+const momentDiff = (startTimestamp, endTimestamp) => {
+  const startMoment = moment(startTimestamp, 'MMMM Do YYYY, h:mm:ss a');
+  const endMoment = moment(endTimestamp, 'MMMM Do YYYY, h:mm:ss a');
+  return endMoment.diff(startMoment, 'seconds');
 }
 
 const getSpurts = (activities, category) => {
@@ -31,8 +18,9 @@ const getSpurts = (activities, category) => {
       return {
         app: activity.app,
         title: activity.title,
-        startTime: spurt.startTime,
-        endTime: spurt.endTime,
+        startTime: timestampToUnix(spurt.startTime),
+        endTime: timestampToUnix(spurt.endTime),
+        duration: momentDiff(spurt.startTime, spurt.endTime),
         productivity_class: activity.productivity.class,
         productivity_source: activity.productivity.source, // in case necessary
       }
@@ -43,111 +31,114 @@ const getSpurts = (activities, category) => {
 
 const getAllSpurts = (activities) => {
   const flattenedSpurts = [...getSpurts(activities, 'productive'), ...getSpurts(activities, 'neutral'), ...getSpurts(activities, 'distracting')]
-  return flattenedSpurts.sort((a, b) => timestampToUnix(a.startTime) - timestampToUnix(b.startTime))
+  return flattenedSpurts.sort((a, b) => a.startTime - b.startTime)
 }
 
-const ChartSpurts = (props) => {
+const mapActivitiesArr = (activitiesArr) => {
+  if (!activitiesArr.length) return [];
+  return activitiesArr.map((activity) => ({
+    name: `${activity.app} - ${activity.title}`,
+    size: activity.duration
+  }));
+}
 
-  // console.log(JSON.stringify(getAllSpurts(props.activities)))
+const treeMapActivities = (activities) => {
+  return [
+    {
+      name: 'distracting',
+      children: mapActivitiesArr(activities.distracting)
+    },
+    {
+      name: 'neutral',
+      children: mapActivitiesArr(activities.neutral)
+    },
+    {
+      name: 'productive',
+      children: mapActivitiesArr(activities.productive)
+    }
+  ];
+}
 
-    let sampleData = [
-    {id: 2, app: "Google Chrome", title: "Label in center of PieChart · Issue #191 · recharts/recharts", spurts: Array(32), duration: 37},
-    {id: 4, app: "Google Chrome", title: "CustomActiveShapePieChart | Recharts", spurts: Array(3), duration: 4},
-    {id: 5, app: "Google Chrome", title: "A pie chart that has customized label - JSFiddle", spurts: Array(7), duration: 557},
-    {id: 9, app: "Slack", title: "Slack - Hack Reactor NYC - Students", spurts: Array(14), duration: 14},
-    ];
+const COLORS = ['#8889DD', '#9597E4', '#8DC77B', '#A5D297', '#E2CF45', '#F8C12D'];
 
-    let sampleSpurts = [
-    {startTime: "May 21st 2018, 7:38:43 pm", endTime: "May 21st 2018, 7:38:44 pm"},
-    {startTime: "May 21st 2018, 7:38:43 pm", endTime: "May 21st 2018, 7:38:44 pm"},
-    {startTime: "May 21st 2018, 7:38:43 pm", endTime: "May 21st 2018, 7:38:44 pm"}
-    ];
+class CustomizedContent extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { root, depth, x, y, width, height, index, payload, colors, rank, name } = this.props;
+    return (
+      <g>
+        <rect
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          style={{
+            fill: depth < 2 ? colors[Math.floor(index / root.children.length * 6)] : 'none',
+            stroke: '#fff',
+            strokeWidth: 2 / (depth + 1e-10),
+            strokeOpacity: 1 / (depth + 1e-10),
+          }}
+        />
+        {
+          depth === 1 ?
+          <text
+            x={x + width / 2}
+            y={y + height / 2 + 7}
+            textAnchor="middle"
+            fill="#fff"
+            fontSize={14}
+          >
+            {name}
+          </text>
+          : null
+        }
+        {
+          depth === 1 ?
+          <text
+            x={x + 4}
+            y={y + 18}
+            fill="#fff"
+            fontSize={16}
+            fillOpacity={0.9}
+          >
+            {index + 1}
+          </text>
+          : null
+        }
+      </g>
+    );
+  }
+};
 
-    //make the spurts data 1 object with keys, and each key represents a bar/spurt
-    const mappedSpurts = [
-      {name: '11am', uv: 280, pv: 450, amt: 360},
-    ];
+class ChartSpurts extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+  }
+  render() {
+    console.log('activities are', this.props.activities)
+    const data = treeMapActivities(this.props.activities)
+    console.log('data is', data)
 
     return (
-    <div style={{marginTop: '15px'}}>
-      <h3>Your Timeline</h3>
-      <BarChart 
-        width={700} 
-        height={65} 
-        data={data} 
-        layout="vertical"
-        margin={{top: 5, right: 30, left: 0, bottom: 5}}
-      >
-        <XAxis axisLine={false} type="number"
-          ticks={[600, 1200, 1800, 2400, 3000, 3600]}
-          tickFormatter={formatXAxis}  
-        />
-        <YAxis tickLine={false} type="category" dataKey="name" />
-        <CartesianGrid horizontal={false} verticalFill={['#FFF']} />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#82ca9d" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-      </BarChart>
-      
-      <BarChart 
-        width={600} 
-        height={65} 
-        data={data2} 
-        layout="vertical"
-        margin={{top: 5, right: 30, left: 0, bottom: 5}}
-      >
-        <XAxis axisLine={false} type="number"
-          ticks={[600, 1200, 1800, 2400, 3000, 3600]}
-          tickFormatter={formatXAxis}  
-        />
-        <YAxis tickLine={false} type="category" dataKey="name" />
-        <CartesianGrid horizontal={false} verticalFill={['#FFF']} />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#8884d8" />
-        
-        <Bar dataKey="uv" stackId='1' fill="#82ca9d" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-      </BarChart>
-      
-      <BarChart 
-        width={600} 
-        height={65} 
-        data={data3} 
-        layout="vertical"
-        margin={{top: 5, right: 30, left: 0, bottom: 5}}
-      >
-        <XAxis axisLine={false} type="number"
-          ticks={[600, 1200, 1800, 2400, 3000, 3600]}
-          tickFormatter={formatXAxis}  
-        />
-        <YAxis tickLine={false} type="category" dataKey="name" />
-        <CartesianGrid horizontal={false} verticalFill={['#FFF']} />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="pv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#8884d8" />
-        <Bar dataKey="uv" stackId='1' fill="#82ca9d" />
-        <Bar dataKey="uv" stackId='1' fill="#ffcc33" />
-      </BarChart>
-    </div>
+      <Treemap
+      width={400}
+      height={200}
+      data={data}
+      dataKey="size"
+      ratio={4/3}
+      stroke="#fff"
+      fill="#8884d8"
+      content={<CustomizedContent colors={COLORS}/>}
+      isAnimationActive={true}
+    >
+     <Tooltip></Tooltip>
+    </Treemap>
     );
-}
+  }
+} 
 
 export default ChartSpurts;
